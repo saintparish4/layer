a33285d9d13865dadbc0e4b2e17f5f3029a95bf0
Sharif <saintparish6@gmail.com>
2025-06-27 23:04:02 -0400

    better-auth update

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import prisma from "@/lib/prisma";
+import { PrismaClient } from "@/app/generated/prisma";
+const prisma = new PrismaClient();
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
